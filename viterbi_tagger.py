922181723b9f09193d23d014ab517b918b442ecd
heygent <heygent@users.noreply.github.com>
2019-09-26 12:46:57 +0200

    Classe ViterbiTagger

--- viterbi_tagger.py
+++ viterbi_tagger.py
@@ -1,6 +1,7 @@
 from typing import List
+
+from pos_tagger import PosTagger
 from training import HMM
-import numpy as np
 from pprint import pprint
 
 
@@ -8,68 +9,74 @@ def make_default_emissions(pos_list):
 
     # vit = {pos: 1 / len(pos_list) for pos in pos_list}
 
-    vit ={}
+    vit = {}
 
     vit["PROPN"] = 1
 
     return vit
 
 
-viterbi_matrix = np.matrix
+class ViterbiTagger(PosTagger):
+    def __init__(self, hmm: HMM):
+        self.transition = hmm.transition
+        self.emission = hmm.emission
 
+    def pos_tag(self, tokens: List[str]):
+        emissions = self.emission
+        transitions = self.transition
+        default_emissions = make_default_emissions(transitions.keys())
 
-def viterbi(hmm: HMM, tokens: List[str]):
+        # pprint(transitions)
+        # pprint(emissions)
 
-    emissions = hmm.emission
-    transitions = hmm.transition
-    default_emissions = make_default_emissions(transitions.keys())
+        viterbi_matrix = [
+            {
+                pos: em_value * transitions["Q0"][pos]
+                for pos, em_value in emissions.get(
+                    tokens[0], default_emissions
+                ).items()
+            }
+        ]
+        pprint(viterbi_matrix)
 
-    # pprint(transitions)
-    # pprint(emissions)
+        for token in tokens[1:]:
+            previus_column = len(viterbi_matrix) - 1
 
-    viterbi_matrix = [
-        {
-            pos: em_value * transitions["Q0"][pos]
-            for pos, em_value in emissions.get(tokens[0], default_emissions).items()
-        }
-    ]
-    pprint(viterbi_matrix)
+            for previus_pos, previus_value in viterbi_matrix[
+                previus_column
+            ].items():
 
-    for token in tokens[1:]:
-        previus_column = len(viterbi_matrix) - 1
+                viterbi_matrix = [
+                    {
+                        pos: em_value
+                        * transitions[previus_pos][pos]
+                        * previus_value
+                        for pos, em_value in emissions.get(
+                            token, default_emissions
+                        ).items()
+                    }
+                ]
 
-        for previus_pos, previus_value in viterbi_matrix[previus_column].items():
+        # viterbi_matrix = [
+        #     {
+        #         pos: em_value * transitions[previus_pos][pos] * previus_value
+        #         for pos, em_value in emissions.get(token, default_emissions).items()  # transition_qualcosa
+        #     }
+        # ]
 
-            viterbi_matrix = [
-                {
-                    pos: em_value * transitions[previus_pos][pos] * previus_value
-                    for pos, em_value in emissions.get(token, default_emissions).items()
-                }
-            ]
-
-
-
-
-    # viterbi_matrix = [
-    #     {
-    #         pos: em_value * transitions[previus_pos][pos] * previus_value
-    #         for pos, em_value in emissions.get(token, default_emissions).items()  # transition_qualcosa
-    #     }
-    # ]
-
-    # pprint(transitions)
-    # pprint(emissions)
-    # for value in transitions.get["Qf"]("NOUN") : print(value)
-    # for key,value in emissions.get(tokens[0]).items() : print(key,value)
-    # pprint(transitions)
-    # print(transitions["X"]["ADJ"])
-    # for value in emissions.get(tokens[0]).values(): print(value)
-    # print(emissions.get(tokens[0]).values())
+        # pprint(transitions)
+        # pprint(emissions)
+        # for value in transitions.get["Qf"]("NOUN") : print(value)
+        # for key,value in emissions.get(tokens[0]).items() : print(key,value)
+        # pprint(transitions)
+        # print(transitions["X"]["ADJ"])
+        # for value in emissions.get(tokens[0]).values(): print(value)
+        # print(emissions.get(tokens[0]).values())
 
 
 if __name__ == "__main__":
     from training import hmm_ud_english
     from sentences import tokenized_sentences as sentences
 
-    hmm = hmm_ud_english()
-    viterbi(hmm, sentences[1])
+    tagger = ViterbiTagger(hmm_ud_english())
+    tagger.pos_tag(sentences[1])
