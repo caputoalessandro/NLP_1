060c00b239f3baadd3ba1d1ff288d05e96f2db9a
heygent <emanuele.gentiletti@gmail.com>
2019-10-02 15:58:53 +0200

    Cambia tipo di restituzione del tagger

--- tagger/abc.py
+++ tagger/abc.py
@@ -1,8 +1,8 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Tuple
 
 
 class PosTagger(ABC):
     @abstractmethod
-    def pos_tag(self, tokens: List[str]):
+    def pos_tag(self, tokens: List[str]) -> List[Tuple[str, str]]:
         pass
