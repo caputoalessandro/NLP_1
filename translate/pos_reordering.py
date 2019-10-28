bb8a237a0ef0b50b098f0ea1164b5c6106bed079
heygent <emanuele.gentiletti@gmail.com>
2019-10-28 14:17:26 +0100

    Refactor pos_reordering.py

--- translate/pos_reordering.py
+++ translate/pos_reordering.py
@@ -1,9 +1,9 @@
 from typing import List
+
 from translate.data import Form
-from pprint import pprint
 
 
-def splitter_function(forms : List):
+def pos_reordering(forms: List[Form]):
 
     initial_range = 0
     reordered_sentence = []
@@ -11,23 +11,29 @@ def splitter_function(forms : List):
     for form in forms:
         if form.pos == "ADV" or form.pos == "CCONJ":
             final_range = forms.index(form)
-            reordered_sentence = reordered_sentence + pos_reordering(forms[initial_range:final_range])
+            reordered_sentence = reordered_sentence + pos_reordering_local(
+                forms[initial_range:final_range]
+            )
             initial_range = final_range
 
-    reordered_sentence = reordered_sentence + pos_reordering(forms[initial_range:])
+    reordered_sentence = reordered_sentence + pos_reordering_local(
+        forms[initial_range:]
+    )
 
-    pprint(reordered_sentence)
     return reordered_sentence
 
 
-def pos_reordering(forms: List[Form]):
+def pos_reordering_local(forms: List[Form]):
     forms = forms.copy()
     reordered_sentence = [forms[0]]
     forms.remove(forms[0])
 
     while forms:
 
-        if reordered_sentence[-1].pos == "DET" or reordered_sentence[-1].pos == "VERB":
+        if (
+            reordered_sentence[-1].pos == "DET"
+            or reordered_sentence[-1].pos == "VERB"
+        ):
             chosen_form = list(filter(lambda x: x.pos == "NOUN", forms))
             if chosen_form:
                 reordered_sentence.append(chosen_form[0])
@@ -38,7 +44,7 @@ def pos_reordering(forms: List[Form]):
                 forms.remove(forms[0])
 
         elif reordered_sentence[-1].pos == "NOUN":
-            chosen_form = list(filter(lambda x: x.pos == 'X', forms))
+            chosen_form = list(filter(lambda x: x.pos == "X", forms))
             if chosen_form:
                 reordered_sentence.append(chosen_form[0])
                 forms.remove(chosen_form[0])
@@ -52,4 +58,3 @@ def pos_reordering(forms: List[Form]):
             forms.remove(forms[0])
 
     return reordered_sentence
-
