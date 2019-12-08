9adae45425d8151fb55b21e8ae43765e0e6d0f2f
Ale Cap <--global>
2019-12-08 13:03:28 +0100

    viterbi 60%

--- tagger/viterbi_tagger.py
+++ tagger/viterbi_tagger.py
@@ -4,6 +4,7 @@ from resources import tokenized_sentences as sentences
 from tagger.abc import PosTagger
 from tagger.hmm import HMM
 from tagger.hmm import hmm_ud_english
+from pprint import pprint
 
 
 class ViterbiTagger(PosTagger):
@@ -11,19 +12,23 @@ class ViterbiTagger(PosTagger):
         self.hmm = hmm
 
     def pos_tag(self, tokens: List[str]):
+
         transitions, emissions, default_emissions = self.hmm
-        dict_to_add = {}
-        backpointer = []
+        viterbi_to_add = {}
+        path_to_add = {}
+        values = []
 
         # prima parola
         viterbi_matrix = [
             {
-                pos: transitions["Q0"][pos] * em_value
+                pos: transitions["Q0"][pos] + em_value
                 for pos, em_value in emissions.get(
                     tokens[0], default_emissions
                 ).items()
             }
         ]
+
+        backpointer = [{max(viterbi_matrix[0].keys()): "Q0"}]
 
         # parole centrali
         for token in tokens[1:]:
@@ -32,63 +37,51 @@ class ViterbiTagger(PosTagger):
                 token, default_emissions
             ).items():
 
-                to_add = [
-                    (
-                        pos,
-                        em_value
-                        * transitions.get(previus_pos, {}).get(pos, 0)
-                        * previus_value,
+                for previus_pos, previus_value in viterbi_matrix[-1].items():
+
+                    values.append(
+                        (
+                            previus_pos,
+                            em_value
+                            + transitions.get(previus_pos, {}).get(pos, 0)
+                            + previus_value,
+                        )
                     )
-                    for previus_pos, previus_value in viterbi_matrix[
-                        -1
-                    ].items()
-                ]
 
-                value_to_add = max(to_add, key=lambda x: x[1])
-                dict_to_add[value_to_add[0]] = value_to_add[1]
+                previus_pos, value = max(values)
+                viterbi_to_add[pos] = value
+                path_to_add[pos] = previus_pos
+                values = []
 
-                add_to_path = [
-                    (
-                        previus_pos,
-                        previus_value
-                        * transitions.get(previus_pos, {}).get(pos, 0),
-                    )
-                    for previus_pos, previus_value in viterbi_matrix[
-                        -1
-                    ].items()
-                ]
-
-            viterbi_matrix.append(dict_to_add)
-            pos_to_add = max(add_to_path, key=lambda x: x[1])
-            # tuple_to_add = (token, pos_to_add[0])
-            backpointer.append(pos_to_add[0])
-            dict_to_add = {}
+            viterbi_matrix.append(viterbi_to_add)
+            backpointer.append(path_to_add)
+            viterbi_to_add = {}
+            path_to_add = {}
 
         # ultima parola
-        to_add = [
-            (
-                pos,
-                previus_value * transitions.get(previus_pos, {}).get("Qf", 0),
-            )
-            for previus_pos, previus_value in viterbi_matrix[-1].items()
-        ]
-
-        value_to_add = max(to_add, key=lambda x: x[1])
-        dict_to_add[value_to_add[0]] = value_to_add[1]
-        viterbi_matrix.append(to_add)
-
-        add_to_path = [
+        previus_pos, value = max(
             (
                 previus_pos,
-                previus_value * transitions.get(previus_pos, {}).get("Qf", 0),
+                previus_value + transitions.get(previus_pos, {}).get("Qf", 0),
             )
-            for previus_pos, previus_value in viterbi_matrix[-1]
-        ]
+            for previus_pos, previus_value in viterbi_matrix[-1].items()
+        )
 
-        pos_to_add = max(add_to_path, key=lambda x: x[1])
-        backpointer.append(pos_to_add[0])
-        res = list(zip(tokens, backpointer))
-        return res
+        viterbi_matrix.append({pos: value})
+        backpointer.append({"Qf": previus_pos})
+
+        rev_backpointer = reversed(backpointer)
+        next(rev_backpointer)
+        previus_pos = backpointer[-1].get("Qf")
+        path = [previus_pos]
+
+        for pointer in rev_backpointer:
+            if pointer.get(previus_pos) == "Q0":
+                break
+            path.append(pointer.get(previus_pos))
+            previus_pos = pointer.get(previus_pos)
+
+        return list(zip(tokens, reversed(path)))
 
 
 def ud_viterbi_tagger():
@@ -96,6 +89,5 @@ def ud_viterbi_tagger():
 
 
 if __name__ == "__main__":
-
     tagger = ud_viterbi_tagger()
     tagger.pos_tag(sentences[0])
