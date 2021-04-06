570f6506e7987459eae80bad2eca0e5f632ddd25
heygent <emanuele.gentiletti@gmail.com>
2021-04-06 19:15:21 +0200

    Adds most common errors

--- tagger/evaluation.py
+++ tagger/evaluation.py
@@ -1,54 +1,107 @@
+from collections import Counter
+
+from tabulate import tabulate
+
 from resources import Corpus
 from tagger.abc import PosTagger
 
 
-def correct_tags_count_in_sentence(tagger: PosTagger, sentence):
-    golden = [
-        (word.form, word.upos) for word in sentence if not word.is_multiword()
-    ]
+def sentence_errors(tagger: PosTagger, golden):
+    errors = Counter()
     tokens = [token for token, _ in golden]
-    hypothesis = tagger.tagged_tokens(tokens)
+    hypothesis = tagger.pos_tags(tokens)
 
     assert len(golden) == len(hypothesis)
-    return len(tokens), sum(g == h for g, h in zip(golden, hypothesis))
+
+    for (_, correct), prediction in zip(golden, hypothesis):
+        if prediction != correct:
+            errors[(prediction, correct)] += 1
+
+    return errors
 
 
-def correct_tags_ratio_in_corpus(tagger: PosTagger, corpus: Corpus):
-    total_tags = 0
-    correct_tags = 0
+def corpus_errors(tagger: PosTagger, corpus: Corpus):
+    errors = Counter()
 
     for sentence in corpus.test:
-        sentence_tags, correct_sent_tags = correct_tags_count_in_sentence(
-            tagger, sentence
-        )
-        total_tags += sentence_tags
-        correct_tags += correct_sent_tags
+        tagged_tokens = [
+            (word.form, word.upos) for word in sentence if not word.is_multiword()
+        ]
+        errors += sentence_errors(tagger, tagged_tokens)
 
-    return correct_tags / total_tags
+    return errors
 
 
-def test_performance(corpus: Corpus, taggers: list[tuple[str, PosTagger]]):
-    for name, tagger in taggers:
-        performance = correct_tags_ratio_in_corpus(tagger, corpus)
-        print(f"{name}: {performance:.4%}")
+def total_test_tokens(corpus):
+    return sum(
+        1 for sentence in corpus.test for word in sentence if not word.is_multiword()
+    )
+
+
+def accuracy(all_errors: Counter, total_tags: int):
+    return sum(all_errors.values()) / total_tags
+
+
+def format_errors(errors: Counter):
+    return [
+        f".{count:>5} \u2716 {pred} \u2713 {correct}"
+        for (pred, correct), count in errors.most_common(5)
+    ]
 
 
 def main():
     from tagger import BaselineTagger, HMMTagger
-    from tagger.smoothing import ALWAYS_NOUN, ALWAYS_NOUN_OR_VERB, UNIFORM, probability_of_occurring_once
+    from tagger.smoothing import (
+        ALWAYS_NOUN,
+        NOUN_OR_VERB,
+        UNIFORM,
+        probability_of_occurring_once,
+    )
 
     for corpus in (Corpus.latin(), Corpus.greek()):
         hmm = HMMTagger.train(corpus)
         taggers = [
-            ('Baseline', BaselineTagger.train(corpus)),
-            ('HMM with NOUN', hmm.with_unknown_emissions(ALWAYS_NOUN)),
-            ('HMM with NOUN|VERB', hmm.with_unknown_emissions(ALWAYS_NOUN_OR_VERB)),
-            ('HMM with uniform', hmm.with_unknown_emissions(UNIFORM)),
-            ('HMM with stats', hmm.with_unknown_emissions(probability_of_occurring_once(corpus)))
+            BaselineTagger.train(corpus),
+            hmm.with_unknown_emissions(ALWAYS_NOUN),
+            hmm.with_unknown_emissions(NOUN_OR_VERB),
+            hmm.with_unknown_emissions(UNIFORM),
+            hmm.with_unknown_emissions(probability_of_occurring_once(corpus)),
         ]
 
-        print(f"\nCorpus {corpus.name}")
-        test_performance(corpus, taggers)
+        tagger_names = [
+            "Baseline",
+            "HMM: Always NOUN",
+            "HMM: 0.5 NOUN, 0.5 VERB",
+            "HMM: 1/#PosTags",
+            "HMM: Stats on occurring once",
+        ]
+
+        tagger_errors = [corpus_errors(tagger, corpus) for tagger in taggers]
+        total_tokens = total_test_tokens(corpus)
+        tagger_accuracies = [
+            (total_tokens - sum(errors.values())) / total_tokens
+            for errors in tagger_errors
+        ]
+
+        print(f"\nCorpus {corpus.name}\n")
+        print(
+            tabulate(
+                zip(tagger_names, tagger_accuracies),
+                headers=("Tagger", "Accuracy"),
+                floatfmt=".2%",
+            )
+        )
+
+        print("\nMost common errors\n")
+        print(
+            tabulate(
+                {
+                    name: format_errors(errors)
+                    for name, errors in zip(tagger_names, tagger_errors)
+                },
+                headers="keys",
+            )
+        )
 
 
 if __name__ == "__main__":
